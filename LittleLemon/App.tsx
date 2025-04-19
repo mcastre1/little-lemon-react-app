4412791b799ac5615f71f53ccc806566a88a5c19
= <miguelcastrejongal@gmail.com>
2025-04-18 21:56:45 -0600

    Created navigation stack and added a profile page.

--- LittleLemon/App.tsx
+++ LittleLemon/App.tsx
@@ -1,12 +1,21 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import OnBoarding from './screens/Onboarding';
+import Profile from './screens/Profile';
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="OnBoarding" component={OnBoarding}/>
+        <Stack.Screen name="Profile" component={Profile}/>
+      </Stack.Navigator>
+    </NavigationContainer>
+
   );
 }
 
@@ -14,7 +23,5 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
 });
---
+++ LittleLemon/screens/Onboarding.js
@@ -0,0 +1,92 @@
+import React from "react";
+import { View, Text, SafeAreaView, TouchableOpacity, TextInput } from 'react-native';
+import { StyleSheet } from "react-native";
+
+import { Dimensions } from 'react-native';
+
+const screenHeight = Dimensions.get('window').height;
+const screenWidth = Dimensions.get('window').width;
+
+function OnBoarding() {
+    return (
+        <SafeAreaView style={styles.container}>
+            {/* Header */}
+            <View style={styles.header}>
+                <Text style={styles.headerText}>Little Lemon</Text>
+            </View>
+
+            {/* Main Content */}
+            <View style={styles.content}>
+                <Text style={{ marginBottom: 100 }}>Let us get to know you</Text>
+                <Text>First Name</Text>
+                <TextInput style={styles.input}></TextInput>
+                <Text>Email</Text>
+                <TextInput style={styles.input}></TextInput>
+            </View>
+
+            {/* Footer */}
+            <View style={styles.footer}>
+                <TouchableOpacity style={styles.footerButton}>
+                    <Text style={styles.buttonText}>Next</Text>
+                </TouchableOpacity>
+
+            </View>
+        </SafeAreaView>
+
+    );
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+    },
+    header: {
+        flex: 1,
+        padding: 20,
+        backgroundColor: '#859194',
+        justifyContent: 'center',
+        alignItems: 'center'
+    },
+    headerText: {
+        color: 'white',
+        fontSize: 20,
+        fontWeight: 'bold',
+    },
+    content: {
+        flex: 6,
+        padding: 20,
+        backgroundColor: '#6A7477',
+        alignItems: 'center'
+    },
+    footer: {
+        flex: 2,
+        padding: 15,
+        backgroundColor: '#9DADB1',
+        justifyContent: 'center',
+        alignItems: 'flex-end'
+    },
+    footerButton: {
+        width: 100,       // or '50%' for responsive
+        height: 50,       // fixed height
+        backgroundColor: '#007AFF',
+        justifyContent: 'center',
+        alignItems: 'center',
+        borderRadius: 8,
+      },
+      buttonText: {
+        color: '#fff',
+        fontSize: 16,
+      },
+      input: {
+        height: 50,
+        borderColor: '#999',
+        borderWidth: 1,
+        borderRadius: 8,
+        paddingHorizontal: 15,
+        fontSize: 16,
+        backgroundColor: '#fff',
+      },
+});
+
+export default OnBoarding;
